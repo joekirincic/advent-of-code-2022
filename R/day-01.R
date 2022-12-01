39cd6d5731bdf8a1fe53c405d185eb2f7353d1ba
joekirincic <jdkirincic@gmail.com>
2022-12-01 09:20:26 -0500

    Linked to sol'n found on Twitter.

--- R/day-01.R
+++ R/day-01.R
@@ -21,7 +21,8 @@ answers <- summarize(
   sum_top_three_highest_calories = sum(calories[1:3], na.rm = TRUE)
 )
 
-# The following riffs off Antoine Fabri's more elegant solution using `tapply`
+# The following riffs off Antoine Fabri's more elegant solution using `tapply`.
+# (See https://twitter.com/antoine_fabri/status/1598243753663549440?s=46&t=0sm3MLtB6gN33_kNtTkjbQ)
 # (Total lines: 9).
 df <- parse_double(read_lines(file = "data/day-01.txt")) %>%
   tapply(X = ., INDEX = cumsum(is.na(.)), FUN = sum, na.rm = TRUE) %>%
